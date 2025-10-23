49b0dbd003724299cfb1bcc837ef6346bb7d6e2c
Sulthan Zahran <SulthanZahran91@ui.ac.id>
2025-10-23 22:44:23 +0700

    tes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+
+const debugIntegration = () => ({
+  name: "debug-integration",
+  hooks: {
+    "astro:config:done": ({ config }) => {
+      console.log("DEBUG adapter", config.adapter?.name);
+    },
+  },
+});
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-  output: "static",
+  integrations: [cloudflare(), mdx(), sitemap(), debugIntegration()],
 });
